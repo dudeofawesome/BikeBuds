11240f83f52debd01981835eeb3401cd9285e47b
Louis Orleans <louis@0rleans.com>
2015-02-25 00:58:36 -0800

    Adding date logging to PartyServer logger

--- Hardware/PartyServer.js
+++ Hardware/PartyServer.js
@@ -10,8 +10,9 @@ var io = require('socket.io')(server);
 var util = require('util');
 var log_file = fs.createWriteStream(__dirname + '/logs/debug.log', {flags : 'w'});
 var log_stdout = process.stdout;
+var date = new Date();
 console.log = function(d) {
-	log_file.write(util.format(d) + '\n');
+	log_file.write(date.toString() + ": " + util.format(d) + '\n');
 	log_stdout.write(util.format(d) + '\n');
 };
 var DATE = new Date();
